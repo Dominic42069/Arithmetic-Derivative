500fe6d8d80752eab666c94ce67370cb8edd5fe1
Dominic Whitaker <dominic.whitaker20@imperial.ac.uk>
2022-05-21 21:59:35 +0100

    implemented arithmetic derivative for integers

---
+++ arithmetic_derivative.py
@@ -0,0 +1,82 @@
+from numbers import Number
+from numpy import sqrt
+
+class PrimeFactoredInteger:
+    def primefactorise(self):
+        n = self.abs_n
+        primes = []
+        powers = []
+        while n % 2 == 0:
+            primes.append(2)
+            n /= 2
+        for k in range(3, int(sqrt(self.abs_n))+1, 2):
+            while n % k == 0:
+                primes.append(k)
+                n /= k
+        for i in set(primes):
+            powers.append(primes.count(i))
+        primes = list(set(primes))
+        factorisation = list(zip(primes, powers))
+        return factorisation
+
+    def __init__(self, n):
+        if isinstance(n, Number):
+            if n < 0:
+                self.minus = True
+                self.abs_n = -n
+            else:
+                self.abs_n = n
+                self.minus = False
+            self.factorisation = self.primefactorise()
+        else:
+            self.factorisation = n
+            self.minus = False
+            self.abs_n = self.evaluate()
+
+    def evaluate(self):
+        val = 0
+        for factor in self.factorisation:
+            val += factor[0]**factor[1]
+        return val
+
+    def __repr__(self):
+        output = ""
+        if self.minus:
+            output += "-"
+        for factor in self.factorisation:
+            if factor[1] == 1:
+                output += f"{factor[0]} * "
+            else:
+                output += f"{factor[0]}^{factor[1]} * "
+        output = output[:-3]
+        return output
+
+    def __mul__(self, other):
+        return self.abs_n*other
+
+    def __rmul__(self, other):
+        return other*self.abs_n
+
+
+class IntegerWithDerivative(PrimeFactoredInteger):
+    def __init__(self, n):
+        super().__init__(n)
+
+    def derivative(self):
+        if self.minus:
+            m = IntegerWithDerivative(self.abs_n)
+            return -m.derivative()
+        elif self.abs_n == 0 or self.abs_n == 1:
+            return 0
+        else:
+            end_factor = self.factorisation.pop()
+            if not self.factorisation:
+                return end_factor[1]*end_factor[0]**(end_factor[1]-1)
+            else:
+                nfac = list()
+                nfac.append(end_factor)
+                n = IntegerWithDerivative(nfac)
+                m = IntegerWithDerivative(self.factorisation)
+                nder = n.derivative()
+                mder = m.derivative()
+                return mder*n + m*nder
